3ea8c0def5636aacf8e188da1e230e753d773177
dhogate <121538943+dhogate@users.noreply.github.com>
2025-03-31 11:28:15 -1000

    plant team plant log code outline file

--- plantteam_measure.py
+++ plantteam_measure.py
@@ -7,35 +7,43 @@ from bme280 import BME280
 #packages to handle the 
 from datetime import datetime
 import time
+#convert the voltage from the soil sensor from analog to digital using another script
+import sampleanalogtodigital
 
 #set up the gpio ports for sensors
 sensorBME = BME280(address=0x76)
 #sensorsoil = VH400()
 
 # Create a file to log data.
-f = open('weather_log.csv', 'w')
-f.write("timestamp, temperature, pressure")
+f = open('plant_log.csv', 'w')
+f.write("timestamp, temperature, humidity")
 f.flush()
-counter = 0
 
-#convert the voltage from the soil sensor from analog to digital
-
+#Function to plot the data 
+def plantplot(filelog, param, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    ax.set_xlabel(f"Time")
+    ax.set_ylabel(f"{param}")
+    return ax.plot(filelog['timestamp'], filelog[param])
 
 #loop to write the data at specified intervals
 while True:
     #adds the timestamp
     current_time = datetime.now()
     formatted_time = current_time.strftime("%Y/%m/%d %H:%M:%S")
-    r = sensorBME.read ()
+    rBME = sensorBME.read()
+    #rSoil = sensorsoil.read()
     #write the data to a csv
-    line = '\n{},{:.4f},{:.4f}'.format(formatted_time,r['t'],r['p'])
+    line = '\n{},{:.4f},{:.4f}'.format(formatted_time,rBME['t']""", rSoil""")
     print(line)
     f.write(line)
     f.flush()
-    counter += 1
     #time interval between reads
     time.sleep(360)
     
-    
-#Plot the data to a dataframe/csv 
-plt.plot(f['timestamp'], f['temperature'])
+#create plot of humidity vs time and humidity vs time
+fig, axs = plt.subplots(1,2)
+plt.suptitle("Plant Log")
+axs[0] = plantplot(f, 'temperature', axs=[0])
+axs[1] = plantplot(f, 'humidity', axs=[1])
